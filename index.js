c628a187626fa731ffd269f064ee62029f7679c2
sonu7524 <sonudaryani135@gmail.com>
2023-12-13 13:25:42 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
